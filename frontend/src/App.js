d396dd0b925f38483c4201d9cce1b93ed879637c
Willylaisa <laisawilly7@gmail.com>
2023-07-29 02:51:04 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome.js";
+import Signup from "./pages/Signup.js";
+import Tasks from "./pages/Tasks.js";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Welcome />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/logout" element={<Welcome />}/>
+        <Route path="/tasks/:id" element={<Tasks />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
